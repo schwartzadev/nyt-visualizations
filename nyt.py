798d140d6b1f301e504f9c3666da0260e1f6feec
Andrew Schwartz <andrew@aschwartz.me>
2017-06-02 22:16:37 -0600

    update for new seperate files

--- nyt.py
+++ nyt.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
+import api
+import words
+import frequency
 
 import requests
 import time
-from datetime import datetime
-import numpy as np
-import pylab as pl
-from collections import Counter
 
 '''
  * get a key at developer.nytimes.com
@@ -18,7 +17,7 @@ def getNewest(num): # returns the most recent articles from the NYT api
 		r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?' + 
 			'fl=source,snippet,headline,web_url,pub_date&' +
 			'page=' + str(i) + '&' +
-			'api-key=' + apiKey)
+			'api-key=' + api.key)
 		data = r.json()
 		try:
 			print(data['message'])
@@ -28,87 +27,6 @@ def getNewest(num): # returns the most recent articles from the NYT api
 				n += 1
 		time.sleep(.9)
 
-def setApiKey(): # sets the api key, from api.txt
-	with open('api.txt', 'r') as myfile:
-		data = myfile.read()
-		return data
-
-def getFrequency(term, yr):
-	data = {'api-key': apiKey,
-	'fq' : 'headline:(\"' + term + '\")',
-	'fl' : 'headline,snippet,pub_date',
-  	'begin_date' : str(yr) + '0101',
-  	'end_date' : str(yr) + '1231',
-  	'sort' : 'oldest'}
-	r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json', params=data)
-	d = r.json()
-	try:
-		print(d['message'])
-		return None
-	except Exception as e:
-		'''for doc in d['response']['docs']:
-			print(doc['headline']['main'])
-		# this prints each of the articles in the given year'''
-		print(str(yr) + ': ' + str(d['response']['meta']['hits']) + ' hits')
-		return d['response']['meta']['hits']
-
-def graphFrequency(query, startYr, endYr):
-	x = []
-	y = []
-	for i in range(startYr, endYr+1):
-		y.append(getFrequency(query, i))
-		x.append(i)
-		time.sleep(.8) # prevents API limit, adjust as needed
-	pl.xlabel('Year')
-	pl.ylabel('Frequency of Query')
-	prstr = 'NYT Headlines\' frequency of "%s"' % query
-	pl.title(prstr)
-	pl.grid(True)
-	pl.plot(x, y, 'b*')
-	coefs = np.polyfit(x, y, 6) # number of curves, will frequently return RankWanring
-	y_poly = np.polyval(coefs, x)
-	pl.plot(x, y_poly)
-	name = 'nyt-img/%s--%d-%d.svg' % (query, startYr, endYr)
-	print('saved to ' + name)
-	pl.savefig(name, bbox_inches='tight')
-	pl.show()
-
-def graphWords(term): # bar graph of word frequency in a given query
-	wordList = ''
-	x = []
-	y = []
-	data = {'api-key': apiKey,
-	'fq' : 'headline:(\"' + term + '\")',
-	'fl' : 'headline,snippet,pub_date,lead_paragraph'}
-	r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json', params=data)
-	d = r.json()
-	try:
-		print(d['message']) # if api limit exceeded
-	except KeyError as e:
-		for doc in d['response']['docs']:
-			wordList += ' '
-			wordList += doc['headline']['main']
-			wordList += ' '
-			wordList += doc['lead_paragraph']
-	blacklist = ['the', 'says', 'said', 'with', 'that', 'and', 'for', 'his', 'her']
-	wordList = wordList.lower().replace(':', '').replace(',', '')
-	wordList = Counter(wordList.split()).most_common()
-	for item in wordList:
-		if (item[0] in blacklist) or len(item[0]) < 3 or item[1] < 3:
-			pass
-		else:
-			print(item)
-			x.append(item[0].title())
-			y.append(item[1])
-
-	y_pos = np.arange(len(x))
-	pl.bar(y_pos, y, align='center', alpha=0.5)
-	pl.xticks(y_pos, x, rotation = 315)
-	pl.ylabel('Frequency')
-	pl.title('NYT Word Frequency in query "' + term + '"')
-	pl.show()
-
-
-apiKey = setApiKey() # gets key from file **cannot perform requests without this**
-# graphFrequency('republican', 1920, 1928)
-graphWords('merkel')
+api.set() # gets key from file **cannot perform requests without this**
+# words.graph('putin')
+frequency.graph('google', 1998, 2016)
